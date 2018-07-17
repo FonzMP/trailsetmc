9c48b422cfa1dd371ae1d024c8864a179114629d
fonzmp <fonzmp@gmail.com[color]>
2018-07-17 17:07:30 -0400

    corrected config routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :routes
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
-  post '/logout', to: "sessions#destroy"
+  delete '/logout', to: "sessions#destroy"
 
 end
