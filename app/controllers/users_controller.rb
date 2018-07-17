7f729696109851a4d0388c24a67f6da34bba9ad1
fonzmp <fonzmp@gmail.com[color]>
2018-07-17 12:02:33 -0400

    added basic logic for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,51 @@
 class UsersController < ApplicationController
 
   def index
-    
+    @users = User.all
   end
 
   def new
-
+    @user = User.new
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
   def create
-
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.update(user_params)
+    if @user
+      redirect_to user_path(@user)
+    else
+      redirect_to routes_path
+    end
   end
 
   def destroy
+    @user = User.find(params[:id]).destroy
 
+    redirect_to routes_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :password, :birthdate)
   end
 
 end
