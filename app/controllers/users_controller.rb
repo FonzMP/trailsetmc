dc97f15e7225b6845f9ec6a5b19e05c930a4b7e1
fonzmp <fonzmp@gmail.com[color]>
2018-07-17 17:07:04 -0400

    removed pry

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  
   def index
     @users = User.all
   end
@@ -14,7 +14,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    binding.pry
     if @user.save
       session[:user_id] = @user.id
       
