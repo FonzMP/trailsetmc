c7cfd4d7f566e0d494f71a67e42f63d6d724103f
fonzmp <fonzmp@gmail.com[color]>
2018-07-17 17:07:20 -0400

    moved user helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,4 @@ class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true
   validates :username, :uniqueness => { case_sensitive: false }
-
-  def current_user
-    if session[:user_id]
-      @current_user = User.find(session[:user_id])
-    end
-  end
 end
