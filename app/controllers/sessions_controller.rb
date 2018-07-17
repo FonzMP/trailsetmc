1ee9731742aaad183b80799add4ae5c5c6b00141
fonzmp <fonzmp@gmail.com[color]>
2018-07-17 17:07:57 -0400

    removed pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,18 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    binding.pry
+    if @user
+      session[:user_id] = @user.id
+      
+      redirect_to user_path(@user)
+    else
+      redirect_to login_path
+    end
   end 
 
   def destroy
     session.delete :user_id
+
+    redirect_to routes_path
   end
 end
