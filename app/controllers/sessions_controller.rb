f0f3fffc3164d062393e21b40f4bc9ff70916c83
fonzmp <fonzmp@gmail.com[color]>
2018-07-18 14:24:43 -0400

    working login logic for authenticate issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user
+    user = User.where('username LIKE ?', "%#{params[:username]}%")
+    binding.pry
+
+    if user && user.authenticate(params[:password])
       session[:user_id] = @user.id
       
       redirect_to user_path(@user)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
+
   has_secure_password
+
   validates :username, presence: true
-  validates :username, :uniqueness => { case_sensitive: false }
+  validates :password, length: { minimum: 8 }, on: :create
 
   has_many :rides
   has_many :routes, through: :rides
