63308c9c12a37d78b743451cc59c0bb32a356375
fonzmp <fonzmp@gmail.com[color]>
2018-07-18 14:54:59 -0400

    corrected login to not pull activerecord relation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where('username LIKE ?', "%#{params[:username]}%")
+    @user = User.find_by(username: params[:username])
     binding.pry
-
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       
       redirect_to user_path(@user)
