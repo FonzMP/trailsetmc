5d367690faf9a702c7378743f68eaf0c52927426
fonzmp <fonzmp@gmail.com[color]>
2018-07-19 18:26:20 -0400

    basic layout of ride controller

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,20 @@
 class RidesController < ApplicationController
+  
+  def index
+    @rides = Ride.all
+  end
+
+  def new
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = Ride.create(ride_params)
+  end
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:name)
+  end
 end
