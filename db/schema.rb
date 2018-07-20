4a2ae4aebc445b603e8389ebeea4dee395e25a1d
fonzmp <fonzmp@gmail.com[color]>
2018-07-20 11:42:27 -0400

    refresh db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_20_003229) do
+ActiveRecord::Schema.define(version: 2018_07_17_150930) do
 
   create_table "rides", force: :cascade do |t|
     t.integer "user_id"
@@ -19,16 +19,6 @@ ActiveRecord::Schema.define(version: 2018_07_20_003229) do
     t.float "length"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "rides_trails", force: :cascade do |t|
-    t.integer "ride_id"
-    t.integer "trail_id"
-  end
-
-  create_table "rides_users", force: :cascade do |t|
-    t.integer "ride_id"
-    t.integer "user_id"
   end
 
   create_table "trails", force: :cascade do |t|
